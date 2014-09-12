45786c6f17f8bee0a76e7792a618634e9c8cbada
Byron Villacreses <byronvillacreses@gmail.com>
2014-09-12 18:18:31 -0400

    Configuracion url Limpias 2

--- protected/config/main.php
+++ protected/config/main.php
@@ -10,9 +10,13 @@ return array(
 	'name'=>'Utimpor S.A.',
         'language' => 'es',
         'charset' => 'utf-8', //'ISO-8859-1'
-
+        //'theme' => 'utimporblue',//tema comun
+        //'theme' => 'classic',//tema comun
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>array(
+            'log',
+            'bootstrap'
+        ),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -22,14 +26,15 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'sea2014',//pass para generador
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+                        'generatorPaths' => array(
+                            'bootstrap.gii'
+                        )
 		),
-		*/
 	),
 
 	// application components
@@ -39,29 +44,32 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+		// uncomment the following to enable URLs in path-format
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+                        'showScriptName'=>false,
+                        'urlSuffix'=>'.html',
+                        //'caseSensitive'=>true,//Si los controladores Tienen Mayusculas, Caso COntrario False
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
+		/*'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		),*/
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			//'connectionString' => 'mysql:host=localhost;dbname=sea2010_sisweb',
+                        //'connectionString' => 'mysql:host=192.168.10.101;dbname=appweb_2014',
+                        'connectionString' => 'mysql:host=localhost;dbname=appweb_2014',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'root00',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -87,6 +95,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'webmaster@utimpor.com',
+                'rutaIconos' => '/images/acciones/',
 	),
 );
