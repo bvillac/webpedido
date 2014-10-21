e4ba32e8884f1409582d628166dcad745d93eb4b
root <root@localhost>
2014-10-21 12:23:30 -0500

    Implementacion Pdf

--- protected/controllers/VSDocumentosController.php
+++ protected/controllers/VSDocumentosController.php
@@ -1,8 +1,8 @@
 <?php
 
 class VSDocumentosController extends Controller {
-    
-     private function tipoAprobacion() {
+
+    private function tipoAprobacion() {
         return array(
             //'1' => Yii::t('COMPANIA', 'Send'),
             '2' => Yii::t('COMPANIA', 'Authorized'),
@@ -18,13 +18,31 @@ class VSDocumentosController extends Controller {
             echo CJSON::encode($modelo->mostrarDocumentos($contBuscar));
         }
         $this->titleWindows = Yii::t('COMPANIA', 'Document');
-        
+
         $this->render('index', array(
             //'dataProvider' => $dataProvider,
             'model' => $modelo->mostrarDocumentos($contBuscar),
             'tipoDoc' => $modelo->recuperarTipoDocumentos(),
             'tipoApr' => $this->tipoAprobacion(),
         ));
+    }
+
+    public function actionGenerarPdf() {
+        $modelo = new VSDocumentos();
+        //$model = Productos::model()->findAll(); //Consulta para buscar todos los registros
+        $contBuscar = array();
+        $mPDF1 = Yii::app()->ePdf->mpdf('utf-8', 'A4', '', '', 15, 15, 35, 25, 9, 9, 'P'); //Esto lo pueden configurar como quieren, para eso deben de entrar en la web de MPDF para ver todo lo que permite.
+        $mPDF1->useOnlyCoreFonts = true;
+        $mPDF1->SetTitle("JuzgadoSys - Reporte");
+        $mPDF1->SetAuthor("JuzgadoSys");
+        $mPDF1->SetWatermarkText("JuzgadoSys");
+        $mPDF1->showWatermarkText = true;
+        $mPDF1->watermark_font = 'DejaVuSansCondensed';
+        $mPDF1->watermarkTextAlpha = 0.1;
+        $mPDF1->SetDisplayMode('fullpage');
+        $mPDF1->WriteHTML($this->renderPartial('docPDF', array('model' => $modelo->mostrarDocumentos($contBuscar)), true)); //hacemos un render partial a una vista preparada, en este caso es la vista pdfReport
+        $mPDF1->Output('Reporte_Productos' . date('YmdHis'), 'I');  //Nombre del pdf y parámetro para ver pdf o descargarlo directamente.
+        //exit;
     }
 
 }
---
+++ protected/views/vSDocumentos/docPDF.php
@@ -0,0 +1,16 @@
+
+ <htmlpageheader name="myheader">
+ <table width="100%"><tr>
+ <td width="50%" style="color:#0000BB;"><span style="font-weight: bold; font-size: 14pt;">Juzgado de Paz de Hohenau</span><br />República del Paraguay<br /><span style="font-size: 15pt;">&#9742;</span> 0775-232355</td>
+ <td width="50%" style="text-align: right;"><b>Listado de Productos</b></td>
+ </tr></table>
+ </htmlpageheader>
+ 
+<htmlpagefooter name="myfooter">
+ <div style="border-top: 1px solid #000000; font-size: 9pt; text-align: center; padding-top: 3mm; ">
+ Página {PAGENO} de {nb}
+ </div>
+ </htmlpagefooter>
+ 
+<sethtmlpageheader name="myheader" value="on" show-this-page="1" />
+<sethtmlpagefooter name="myfooter" value="on" />
