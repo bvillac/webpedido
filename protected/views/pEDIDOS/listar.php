1b913be4f09e3fdf2f900566e6ad8cfbdec02158
root <root@developer.utimpor>
2018-11-05 14:42:59 -0500

    actualizacion PBX

--- protected/views/pEDIDOS/listar.php
+++ protected/views/pEDIDOS/listar.php
@@ -2,7 +2,8 @@
 <div class="col-lg-12">
     <?php if($cliID=='4'){//Solo Para CLientes 4 no mostrar  ?>
         <div class="alert alert-info alert-global-notice">
-            Una vez guardado el pedido el cliente tiene un tiempo <strong>máximo de 2 horas</strong> para realizar alguna anulación de la orden, de lo contrario se procederá con la atención de la misma.
+            <strong>Nota: </strong>Una vez guardado el pedido el cliente tiene un tiempo <strong>máximo de 2 horas</strong> 
+            para realizar alguna anulación de la orden a nuestro PBX: <strong>3-810300</strong>, de lo contrario se procederá con la atención de la misma.
         </div>
     <?php }  ?>
     <?php $this->renderPartial('_frm_dataTienda', 
