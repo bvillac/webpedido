e62e4c14bdb894d66991b251d2685da9ec5b7cf2
root <root@developer.utimpor>
2019-11-12 13:28:56 -0500

    Datos nuevo pedido

--- protected/views/pEDIDOS/consultar.php
+++ protected/views/pEDIDOS/consultar.php
@@ -24,7 +24,7 @@ echo CHtml::hiddenField('txth_cliId',$cliID);
 <div class="col-lg-12">
     <div class="">
         <?php //if($cliID!='4'){//Solo Para CLientes 4 no mostrar  ?>
-        <?php echo CHtml::link(Yii::t('CONTROL_ACCIONES', 'Create'), array('pEDIDOS/listar'), array('title' => Yii::t('CONTROL_ACCIONES', 'Create'), 'class' => 'btn btn-primary btn-sm',)); ?>
+        <?php echo CHtml::link(Yii::t('CONTROL_ACCIONES', 'Nuevo Pedido'), array('pEDIDOS/listar'), array('title' => Yii::t('CONTROL_ACCIONES', 'Create'), 'class' => 'btn btn-primary btn-sm',)); ?>
         <?php echo CHtml::link(Yii::t('CONTROL_ACCIONES', 'Edit'), array('tIENDA/update'), array('id' => 'btn_Update', 'name' => 'btn_Update', 'title' => Yii::t('CONTROL_ACCIONES', 'Edit'), 'class' => 'btn btn-primary btn-sm disabled', 'onclick' => 'fun_Update()')); ?>
         <?php echo CHtml::button(Yii::t('CONTROL_ACCIONES', 'Cancel'), array('id' => 'btn_anular', 'name' => 'btn_anular', 'class' => 'btn btn-primary btn-sm disabled', 'onclick' => 'fun_DeletePedido()')); ?>
         <?php //}  ?>
